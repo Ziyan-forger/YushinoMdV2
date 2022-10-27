f56b9b018768d50dfa39ad24af538b9c7bccaac2
Ziyan-forger <111504164+Ziyan-forger@users.noreply.github.com>
2022-10-27 16:41:49 +0700

    Update donasi.js

---
+++ plugins/donasi.js
@@ -0,0 +1,8 @@
+⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ DONASI ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
+global.pdana = '0857 1405 6133'
+global.povo = '-'
+global.pgopay = '-'
+global.plinkaja = '-' //link aja
+global.ppulsa = '0857 1405 6133'
+global.ppulsa2 = '085714056133'
+global.psaweria = 'https://saweria.co/zynnxd'
